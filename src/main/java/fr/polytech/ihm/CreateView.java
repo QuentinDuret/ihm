2e3bf1aeb2c52d37d0565b1c8c40f67e4225e0b3
dq610272 <quentin.duret@etu.unice.fr>
2017-03-14 19:11:16 +0100

    gridpane

--- src/main/java/fr/polytech/ihm/CreateView.java
+++ src/main/java/fr/polytech/ihm/CreateView.java
@@ -6,9 +6,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 
 import javax.swing.*;
+
+import static javafx.geometry.HPos.RIGHT;
+import static javafx.geometry.Pos.CENTER_RIGHT;
 
 /**
  * Created by Quentin on 08/03/2017.
@@ -23,33 +27,33 @@ public class CreateView {
 
     public Scene addCenter() {
 
-        AnchorPane anchorPane = new AnchorPane();
+        GridPane gridPane = new GridPane();
 
         try {
 
             String fxmlFile = "/fxml/Haut_de_page.fxml";
             Parent rootNode = FXMLLoader.load(getClass().getResource(fxmlFile));
-            AnchorPane.setTopAnchor(rootNode,0.0);
-
-            Parent rootNodeTop = FXMLLoader.load(getClass().getResource(nameFileCenter));
-            AnchorPane.setRightAnchor(rootNodeTop,0.0);
-
-            String fxmlFileBottom = "/fxml/Bas_de_page.fxml";
-            Parent rootNodeBottom = FXMLLoader.load(getClass().getResource(fxmlFileBottom));
-            AnchorPane.setBottomAnchor(rootNodeBottom,0.0);
+            gridPane.add(rootNode,0,0);
 
             String fxmlFileLeft = "/fxml/Barre_Recherche.fxml";
             Parent rootNodeLeft = FXMLLoader.load(getClass().getResource(fxmlFileLeft));
+            gridPane.add(rootNodeLeft,0,1);
+
             AnchorPane.setLeftAnchor(rootNodeLeft,0.0);
+            Parent rootNodeTop = FXMLLoader.load(getClass().getResource(nameFileCenter));
+            gridPane.add(rootNodeTop,0,1);
+            gridPane.setHalignment(rootNodeTop,RIGHT);
 
+            String fxmlFileBottom = "/fxml/Bas_de_page.fxml";
+            Parent rootNodeBottom = FXMLLoader.load(getClass().getResource(fxmlFileBottom));
+            gridPane.add(rootNodeBottom,0,2);
 
-            anchorPane.getChildren().addAll(rootNode,rootNodeBottom,rootNodeLeft,rootNodeTop);
         }
         catch (Exception e){
             JOptionPane.showInputDialog(null, e);
         }
 
-        Scene scene = new Scene(anchorPane);
+        Scene scene = new Scene(gridPane);
         scene.getStylesheets().add("/css/presentation.css");
 
         return scene;
